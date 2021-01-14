e52f71f078c7299c52e172c1537439c78bf7207b
ittp <61617660+ittp@users.noreply.github.com>
2021-01-14 18:02:52 +0300

    strapi

--- src/index.js
+++ src/index.js
@@ -1,86 +1,97 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Table } from "antd";
+import reqwest from "reqwest";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Table } from 'antd';
-import reqwest from 'reqwest';
+var axios = require("axios");
+var data = "";
+
+var config = {
+  method: "get",
+  url: "https://api.samodelkin.email/yookassa/list",
+  headers: {},
+  data: data
+};
 
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    sorter: true,
-    render: name => `${name.first} ${name.last}`,
-    width: '20%',
-  },
-  {
-    title: 'Gender',
-    dataIndex: 'gender',
-    filters: [{ text: 'Male', value: 'male' }, { text: 'Female', value: 'female' }],
-    width: '20%',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-  },
+    title: "UUID",
+    dataIndex: "id"
+  }
 ];
 
 class App extends React.Component {
   state = {
     data: [],
     pagination: {},
-    loading: false,
+    loading: false
   };
 
   componentDidMount() {
-    this.fetch();
+    //this.fetch();
+
+    let td = this.fetch();
+
+    console.log(td);
   }
 
   handleTableChange = (pagination, filters, sorter) => {
     const pager = { ...this.state.pagination };
     pager.current = pagination.current;
     this.setState({
-      pagination: pager,
+      pagination: pager
     });
     this.fetch({
       results: pagination.pageSize,
       page: pagination.current,
       sortField: sorter.field,
       sortOrder: sorter.order,
-      ...filters,
+      ...filters
     });
   };
 
   fetch = (params = {}) => {
-    console.log('params:', params);
+    console.log("params:", params);
     this.setState({ loading: true });
-    reqwest({
-      url: 'https://randomuser.me/api',
-      method: 'get',
-      data: {
-        results: 10,
-        ...params,
-      },
-      type: 'json',
-    }).then(data => {
-      const pagination = { ...this.state.pagination };
-      // Read total count from server
-      // pagination.total = data.totalCount;
-      pagination.total = 200;
-      this.setState({
-        loading: false,
-        data: data.results,
-        pagination,
-      });
-    });
+    const pagination = { ...this.state.pagination };
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then((data) => this.setState({ loading: false, pagination, data }));
+
+    // reqwest({
+    //   url: "https://api.samodelkin.email/yookassa/list",
+    //   method: "get",
+    //   data: {
+    //     results: 10,
+    //     ...params
+    //   },
+    //   type: "json"
+    // }).then((data) => {
+    //
+    //   // Read total count from server
+    //   // pagination.total = data.totalCount;
+    //   pagination.total = 200;
+    //   this.setState({
+    //     loading: false,
+    //     data: data.items,
+    //     pagination
+    //   });
+    // });
   };
 
   render() {
     return (
       <Table
         columns={columns}
-        rowKey={record => record.login.uuid}
+        rowKey={(record) => record.id}
         dataSource={this.state.data}
         pagination={this.state.pagination}
         loading={this.state.loading}
@@ -90,5 +101,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('container'));
-          
+ReactDOM.render(<App />, document.getElementById("container"));
